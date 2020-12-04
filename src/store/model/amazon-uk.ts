663be1c230e6a330ae5db4d72e162ef5492f3021
Cristian Harsu <charsu@fundapps.co>
2020-12-04 00:45:43 +0000

    disable amazon-uk links builder

--- src/store/model/amazon-uk.ts
+++ src/store/model/amazon-uk.ts
@@ -69,64 +69,64 @@ export const AmazonUk: Store = {
 			url: 'https://www.amazon.co.uk/dp/B08GD9MNZB'
 		}
 	],
-	linksBuilder: {
-		builder: (docElement, series) => {
-			const productElements = docElement.find(
-				'.s-result-list .s-result-item[data-asin]'
-			);
-			const links: Link[] = [];
-			for (let i = 0; i < productElements.length; i++) {
-				const productElement = productElements.eq(i);
-				const asin = productElement.attr()['data-asin'];
+	// linksBuilder: {
+	// 	builder: (docElement, series) => {
+	// 		const productElements = docElement.find(
+	// 			'.s-result-list .s-result-item[data-asin]'
+	// 		);
+	// 		const links: Link[] = [];
+	// 		for (let i = 0; i < productElements.length; i++) {
+	// 			const productElement = productElements.eq(i);
+	// 			const asin = productElement.attr()['data-asin'];
 
-				if (!asin) {
-					continue;
-				}
+	// 			if (!asin) {
+	// 				continue;
+	// 			}
 
-				const url = `https://www.amazon.co.uk/dp/${asin}/`;
-				const titleElement = productElement
-					.find('.sg-col-inner h2 a.a-text-normal[href] span')
-					.first();
-				const title = titleElement.text().trim();
+	// 			const url = `https://www.amazon.co.uk/dp/${asin}/`;
+	// 			const titleElement = productElement
+	// 				.find('.sg-col-inner h2 a.a-text-normal[href] span')
+	// 				.first();
+	// 			const title = titleElement.text().trim();
 
-				if (!title || !new RegExp(`RTX.*${series}`, 'i').exec(title)) {
-					continue;
-				}
+	// 			if (!title || !new RegExp(`RTX.*${series}`, 'i').exec(title)) {
+	// 				continue;
+	// 			}
 
-				const card = parseCard(title);
+	// 			const card = parseCard(title);
 
-				if (card) {
-					links.push({
-						brand: card.brand as any,
-						cartUrl: `https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=${asin}&Quantity.1=1`,
-						model: card.model,
-						series,
-						url
-					});
-				} else {
-					logger.error(`Failed to parse card: ${title}`, {url});
-				}
-			}
+	// 			if (card) {
+	// 				links.push({
+	// 					brand: card.brand as any,
+	// 					cartUrl: `https://www.amazon.co.uk/gp/aws/cart/add.html?ASIN.1=${asin}&Quantity.1=1`,
+	// 					model: card.model,
+	// 					series,
+	// 					url
+	// 				});
+	// 			} else {
+	// 				logger.error(`Failed to parse card: ${title}`, {url});
+	// 			}
+	// 		}
 
-			return links;
-		},
-		ttl: 300000,
-		urls: [
-			{
-				series: '3080',
-				url: [
-					'https://www.amazon.co.uk/s?k=%2B%22RTX+3080%22+-2080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675291',
-					'https://www.amazon.co.uk/s?k=%2B%22RTX+3080%22+-2080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675594&page=2'
-				]
-			},
-			{
-				series: '3090',
-				url: [
-					'https://www.amazon.co.uk/s?k=%2B%22RTX+3090%22+-3080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675291',
-					'https://www.amazon.co.uk/s?k=%2B%22RTX+3090%22+-3080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675594&page=2'
-				]
-			}
-		]
-	},
+	// 		return links;
+	// 	},
+	// 	ttl: 300000,
+	// 	urls: [
+	// 		{
+	// 			series: '3080',
+	// 			url: [
+	// 				'https://www.amazon.co.uk/s?k=%2B%22RTX+3080%22+-2080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675291',
+	// 				'https://www.amazon.co.uk/s?k=%2B%22RTX+3080%22+-2080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675594&page=2'
+	// 			]
+	// 		},
+	// 		{
+	// 			series: '3090',
+	// 			url: [
+	// 				'https://www.amazon.co.uk/s?k=%2B%22RTX+3090%22+-3080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675291',
+	// 				'https://www.amazon.co.uk/s?k=%2B%22RTX+3090%22+-3080+-GTX&i=computers&rh=n%3A430500031%2Cp_n_availability%3A419162031&s=relevancerank&dc&qid=1601675594&page=2'
+	// 			]
+	// 		}
+	// 	]
+	// },
 	name: 'amazon-uk'
 };
